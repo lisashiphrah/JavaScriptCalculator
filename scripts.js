ff454f7199b14df0704a8a41cd2860a2291e9695
Lisa Shiphrah <lisa.shiphrah@gmail.com>
2016-11-15 17:48:43 +0000

    Minor css changes

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
 	var secondNumber = 0;
 	var clearScreen = false;
 	var operation = '';
+	var end = false;
 
 
 	/**************************************************************
